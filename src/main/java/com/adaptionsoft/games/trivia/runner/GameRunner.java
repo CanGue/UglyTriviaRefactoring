a9d53034bb92130be93936123eb7fc9d4ba05f8c
can <guelercan@outlook.com>
2018-04-15 17:48:29 +0200

    prepare for unit test

--- src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -1,9 +1,10 @@
 
 package com.adaptionsoft.games.trivia.runner;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import com.adaptionsoft.games.uglytrivia.Game;
-
 
 public class GameRunner {
 
@@ -16,21 +17,35 @@ public class GameRunner {
 		aGame.add("Pat");
 		aGame.add("Sue");
 		
+		List<Integer> rollingDice = new ArrayList<Integer>();
+		rollingDice.add(1);
+		rollingDice.add(2);
+		rollingDice.add(5);
+		rollingDice.add(4);
+		
+		List<Integer> wrongRightAnswer = new ArrayList<Integer>();
+		wrongRightAnswer.add(1);
+		wrongRightAnswer.add(2);
+		wrongRightAnswer.add(5);
+		wrongRightAnswer.add(7);
+		
 		Random rand = new Random();
+		int roundCounter = 0;
 	
 		do {
 			
-			aGame.roll(rand.nextInt(5) + 1);
+			aGame.roll(rollingDice.get(roundCounter));
 			
-			if (rand.nextInt(9) == 7) {
+			if (wrongRightAnswer.get(roundCounter) == 7) {
 				notAWinner = aGame.wrongAnswer();
 			} else {
 				notAWinner = aGame.wasCorrectlyAnswered();
 			}
 			
-			
+			roundCounter++;
 			
 		} while (notAWinner);
 		
+		System.out.println(aGame.gameMessage);
 	}
 }
